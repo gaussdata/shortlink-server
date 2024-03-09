8cc6e44b320a1fc0a71428e37e4b94bb6b3d2461
gausszhou <gausszhou@qq.com>
2024-03-09 23:04:25 +0800

    feat: 生成压缩码

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UniqueCode } from './entities/UniqueCode';
+import { UniqueCodeService } from './unique-code.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '192.168.4.198',
+      port: 3306,
+      username: 'root',
+      password:'mysql_ArDdx6',
+      database: 'shortlink',
+      synchronize: true,
+      logging: true,
+      entities: [UniqueCode],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password'
+      }
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UniqueCodeService],
 })
 export class AppModule {}
