738f0ba9b8e5b6e247ae0cecdf0cb98d9492a6a8
gausszhou <gausszhou@qq.com>
2024-03-10 17:44:18 +0800

    feat: 调整 API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -16,18 +16,39 @@ export class AppController {
 
   /**
    * 生成短链接
-   * @param longUrl 
-   * @returns 
+   * @param  longUrl 
+   * @returns  
+   * @example /long?url=<your_long_url>
    */
-  @Get('short')
-  async generateShortUrl(@Query('url') longUrl) {
+  @Get('long')
+  async generateShortUrl(@Query('url') longUrl: string) {
     return this.shortLongMapService.generate(longUrl);
+  }
+
+  /**
+   * 反查长链接
+   * @param code 
+   * @returns 
+   * @example /short?code=<your_short_code>
+   */
+  
+  @Get('short')
+  async findLongUrl(@Query('code') code) {
+    const longUrl = await this.shortLongMapService.getLongUrl(code);
+    if (!longUrl) {
+      throw new BadRequestException('短链不存在');
+    }
+    return {
+      url: longUrl,
+      statusCode: 200
+    }
   }
 
   /**
    * 短链接重定向到长链接
    * @param code 
    * @returns 
+   * @example /6Gk3aA1
    */
   @Get(':code')
   @Redirect()
